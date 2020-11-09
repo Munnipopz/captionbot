6b9522484414b81f346d38a0e0c45ef3e7071b50
Flakes WRLD <71187213+FristyFlakes@users.noreply.github.com>
2020-11-09 12:59:55 +0100

    Update captions.py

--- plugins/captions.py
+++ plugins/captions.py
@@ -36,7 +36,7 @@ async def old(client, message):
         parse_mode="markdown", #also you can set html or none
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton('🦋name🦋', url='https://t.me/url')],
+                [InlineKeyboardButton('', url='')],
             ]  
         )
     )
