d86d5c985309206407919891df27901d6272e7ef
Flakes WRLD <71187213+FristyFlakes@users.noreply.github.com>
2020-11-09 12:54:00 +0100

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,18 @@
 class Translation(object):
-    START_MSG = """<b>Do Not Start This Bot</b>""" 
-    HELP = """Nobody's Gonna Help You 😂"""
+    START_MSG = """`channel button helper`
+   <b>made by @legendoftelegram</b>""" 
+    HELP = """
+👉 <b>ask dev</b>
+<b>Bot developed by</b> @Legendoftelegram"""
     CH_LIST = """<b>empty</b>"""  
-    CAP_TION = """📁 @MODZILLA"""
+    CAP_TION = """
+ ✪༺ ──•◈•─ ─•◈•──༻✪
+ ᴄʜᴀɴɴᴇʟ ➧ @MM_MalluMovies
+ ᴄʜᴀɴɴᴇʟ ➧ @MM_TamilMovies
+ ᴄʜᴀɴɴᴇʟ ➧ @MalluMoviesALL
+ ᴄʜᴀɴɴᴇʟ ➧ @Mallu_Rockers
+ ᴄʜᴀɴɴᴇʟ ➧ @MM_TVSeries
+ ᴄʜᴀɴɴᴇʟ ➧ @MM_NEW
+ ᴄʜᴀɴɴᴇʟ ➧ @MM_OLD
+ ᴄʜᴀɴɴᴇʟ ➧ @MM_Linkz
+🎗️ʝσιи 🎗️ ѕнαяє🎗️ ѕυρρσят🎗️"""
